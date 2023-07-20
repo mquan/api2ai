e9fab4fae592d64f1199892a843cf962df0a8280
Quan Nguyen <mquannie@gmail.com>
2023-07-20 09:47:29 -0700

    Fix linting

--- demo/pages/api/ai.ts
+++ demo/pages/api/ai.ts
@@ -6,7 +6,7 @@ const oasesDirectory = path.join(process.cwd(), "oases");
 const oasFilename = path.join(oasesDirectory, "open-ai.yaml");
 
 const apiAgent = new ApiAgent({
-  apiKey: process.env.OPEN_AI_KEY || "",
+  apiKey: process.env["OPEN_AI_KEY"] || "",
   model: "gpt-3.5-turbo-0613",
   api: oasFilename,
 });
@@ -18,7 +18,7 @@ const handler = async (req, res) => {
   try {
     result = await apiAgent.execute({
       userPrompt,
-      context: { token: process.env.OPEN_AI_KEY },
+      context: { token: process.env["OPEN_AI_KEY"] },
     });
 
     res.status(200).json(result);
