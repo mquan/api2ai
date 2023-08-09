fcdc1561e47f411d189d26725f7897ab68f4d84c
Quan Nguyen <mquannie@gmail.com>
2023-08-09 10:28:59 -0700

    Convert postman collection to OpenAPI Spec

--- core/src/api/oas-loader.ts
+++ core/src/api/oas-loader.ts
@@ -2,6 +2,10 @@ import SwaggerParser from "@apidevtools/swagger-parser";
 import Operation from "./operation";
 import Security from "./security";
 
+import postmanToOpenApi from "postman-to-openapi";
+
+import { promises as fs } from "fs";
+
 const parseSecurities = (api: any) => {
   if (!api.components?.securitySchemes) {
     console.warn("No `securitySchemes` found in this API spec.");
@@ -42,6 +46,40 @@ const selectSecurities = ({
   });
 };
 
+const parseJSON = async (filename: string) => {
+  const rawText: string = await fs.readFile(filename, "utf8");
+
+  try {
+    return JSON.parse(rawText);
+  } catch {
+    return null;
+  }
+};
+
+const isPostmanCollection = async (filename: string) => {
+  // Detect and parse postman collection into OAS or default to SwaggerParser.
+  const json = await parseJSON(filename);
+
+  return !!json?.info?._postman_id;
+};
+
+const parseAPI = async (filename: string) => {
+  const isPostman = await isPostmanCollection(filename);
+
+  if (isPostman) {
+    const oas = await postmanToOpenApi(filename, undefined, {
+      replaceVars: true,
+      outputFormat: "json",
+      operationId: "auto",
+      defaultTag: "General",
+    });
+
+    return JSON.parse(oas);
+  } else {
+    return await SwaggerParser.dereference(filename);
+  }
+};
+
 export const parse = async ({
   filename,
   auth,
@@ -49,7 +87,7 @@ export const parse = async ({
   filename: string;
   auth?: object;
 }) => {
-  const api = await SwaggerParser.dereference(filename);
+  const api = await parseAPI(filename);
   const securities = parseSecurities(api);
 
   const operations: Operation[] = [];
